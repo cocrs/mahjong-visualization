10d5ba1b72814f3e474b3af2927a1a14c4ecec6e
Zovjsra <4703michael@gmail.com>
2020-06-10 11:04:37 +0800

    增加主頁

---
+++ interface/cloud.js
@@ -0,0 +1,166 @@
+function wordCloud(selector) {
+    var fill = d3v3.scale.category20();
+    var fill2 = d3v3.scale.category20b();
+    var fill3 = d3v3.scale.category20c();
+
+    //Construct the word cloud's SVG element
+    var svg = d3v3.select(selector)
+        .select(".cloud")
+        .append("svg")
+        .attr("width", 800)
+        .attr("height", 600)
+        .append("g")
+        .attr("transform", "translate(400,300)");
+
+    //Draw the word cloud
+    function draw(words) {
+        var cloud = svg.selectAll("g text").data(words, function (d) {
+            return d.text;
+        });
+
+        //Entering words
+        cloud
+            .enter()
+            .append("text")
+            //.style("opacity", 0.7)
+            .style("font-family", 'Noto Sans TC')
+            .style("fill", function (d, i) {
+                if (i >= 20) return fill2(i - 20);
+                if (i >= 40) return fill3(i - 40);
+                return fill(i);
+            })
+            .attr("text-anchor", "middle")
+            .attr("font-size", 1)
+            .text(function (d) {
+                return d.text;
+            });
+
+        //Entering and existing words
+        cloud
+            .transition()
+            .duration(600)
+            .style("font-size", function (d) {
+                return d.size + "px";
+            })
+            .attr("transform", function (d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .style("fill-opacity", 1);
+
+        let tooltip = d3
+            .select("body")
+            .append("div")
+            .style("visibility", "hidden")
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("position", "center")
+            .style("padding", "4px")
+            .style("border-width", "2px")
+            .style("border-radius", "3px")
+            .style("display", "block")
+            .style("position", "absolute")
+            .style("text-align", "center")
+            .style("font-size", "20px");
+
+        cloud.on("mouseover", function () {
+            d3v3.select(this)
+                .transition()
+                .style("opacity", 1)
+                .attr("d", function (d) {
+                    label = d.text + "<br/>freq: " + d.freq;
+                });
+            let target = this
+            d3v3.selectAll("text")
+                .transition()
+                .style("opacity", function (t) {
+                    return this == target ? 1 : 0.2
+                });
+        });
+        cloud.on("mousemove", function () {
+            tooltip
+                .html(label)
+                .style("left", d3v3.event.pageX - 20 + "px")
+                .style("top", d3v3.event.pageY + 20 + "px")
+                .style("visibility", "visible");
+        });
+        cloud.on("mouseout", function () {
+            d3v3.selectAll("text")
+                .transition()
+                .style("opacity", 1);
+            //d3v3.select(this).style("opacity", 0.7);
+            tooltip.style("visibility", "hidden");
+        });
+    }
+
+    //Use the module pattern to encapsulate the visualisation code. We'll
+    // expose only the parts that need to be public.
+    return {
+        //Recompute the word cloud for a new set of words. This method will
+        // asycnhronously call draw when the layout has been computed.
+        //The outside world will need to call this function, so make it part
+        // of the wordCloud return value.
+        update: function (dataset, linear) {
+            d3.layout
+                .cloud()
+                .size([800, 600])
+                .words(dataset)
+                .padding(5)
+                .rotate(function () {
+                    return ~~(Math.random() * 2) * 90;
+                })
+                .font('Noto Sans TC')
+                .fontSize(function (d) {
+                    return linear(d.size);
+                })
+                .on("end", draw)
+                .start();
+        },
+    };
+}
+
+//Prepare one of the sample sentences by removing punctuation,
+// creating an array of words and computing a random size attribute.
+function getWords(i) {
+    return words[i]
+        .replace(/[!\.,:;\?]/g, "")
+        .split(" ")
+        .map(function (d) {
+            return { text: d, size: 10 + Math.random() * 60 };
+        });
+}
+
+function getBaseLog(x, y) {
+    return Math.log(y) / Math.log(x);
+}
+
+//This method tells the word cloud to redraw with a new set of words.
+//In reality the new words would probably come from a server request,
+// user input or some other source.
+function showNewWords(vis, i) {
+    i = i || 0;
+    d3v3.json("./yakuFreqFormedSimple.json", (error, data) => {
+        if (error) {
+            console.log(error);
+        } else {
+            dataset = data.map(function (d) {
+                return { text: d.name, size: d.freq, freq: d.freq };
+            });
+            var max = 0;
+            dataset.forEach((elm) => {
+                if (elm.freq > max) max = elm.freq;
+            });
+            console.log(max);
+            var linear = d3v3.scale.linear().domain([1, max]).range([12, 100]);
+
+            //console.log(dataset)
+            vis.update(dataset, linear);
+        }
+    });
+}
+
+//Create a new instance of the word cloud visualisation.
+var myWordCloud = wordCloud("body");
+
+//Start cycling through the demo data
+showNewWords(myWordCloud);
---
+++ interface/linechart.js
@@ -0,0 +1,186 @@
+var LineDataset = { "和了率": [], "平均和了点": [], "和了点期待値": [] };
+
+// data
+d3.json("./agariJunme.json").then(function (json) {
+    for (j in json) {
+        for (d in LineDataset) {
+            console.log(LineDataset[d])
+            LineDataset[d].push({ x_value: json[j]['巡目'], y_value: json[j][d] })
+        }
+    }
+    update_chart("和了率");
+});
+
+var margin = { top: 10, right: 80, bottom: 30, left: 50 },
+    width = 550 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// x axis
+var x = d3.scaleLinear().range([0, width]);
+var xAxis = d3.axisBottom().scale(x);
+svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .attr("class", "xAxis")
+
+// y axis
+var y = d3.scaleLinear().range([height, 50]);
+var yAxis = d3.axisLeft().scale(y);
+svg.append("g")
+    .attr("class", "yAxis")
+
+// title
+svg.append("text")
+    .attr("text-anchor", "middle")
+    .attr("font-weight", "bold")
+    .style("font-size", "20px")
+    .attr("x", (width / 2))
+    .attr("y", (margin.top))
+    .text("麻將折線圖");
+
+// x axis text
+svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .style("font-size", "10px")
+    .attr("x", width + 10)
+    .attr("y", height + 10)
+    .text("巡目");
+
+// y axis text
+var y_label = svg.append('text')
+    .attr("class", "x label")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .style("font-size", "10px")
+    .attr('x', -40)
+    .attr('y', 40)
+
+// update x axis, y axis, line
+function update(data) {
+    // x axis
+    x.domain([1, d3.max(data, function (d) { return d.x_value })]);
+    svg.selectAll(".xAxis").transition()
+        .duration(2000)
+        .call(xAxis);
+
+    // y axis
+    y.domain([0, d3.max(data, function (d) { return d.y_value })]);
+    svg.selectAll(".yAxis")
+        .transition()
+        .duration(2000)
+        .call(yAxis);
+
+    // line
+    var line = svg.selectAll(".line")
+        .data([data], function (d) { return d.x_value });
+
+    line.enter()
+        .append("path")
+        .attr("class", "line")
+        .merge(line)
+        .transition()
+        .duration(2000)
+        .attr("d", d3.line()
+            .x(function (d) { return x(d.x_value); })
+            .y(function (d) { return y(d.y_value); }))
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 2.5)
+}
+
+// update y axis label
+function update_y_label(s) {
+    y_label
+        .transition()
+        .duration(1000)
+        .style("opacity", 0)
+        .transition().duration(500)
+        .style("opacity", 1)
+        .text(s)
+}
+
+function hover(svg, data, start) {
+    if (start === false) {
+        svg.on("mousemove", not_moved)
+            .on("mouseenter", not_entered)
+            .on("mouseleave", not_left);
+        return
+    }
+    if ("ontouchstart" in document)
+        // Touch
+        svg.style("-webkit-tap-highlight-color", "transparent")
+            .on("touchmove", moved)
+            .on("touchstart", entered)
+            .on("touchend", left)
+    else
+        // Mouse
+        svg.on("mousemove", moved)
+            .on("mouseenter", entered)
+            .on("mouseleave", left);
+
+    // Dot
+    const dot = svg.append("g")
+        .attr("display", "none");
+
+    dot.append("circle")
+        .attr("r", 2.5);
+
+    dot.append("text")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .attr("font-weight", "bold")
+        .attr("text-anchor", "start")
+        .attr("y", -8);
+
+    function not_moved() {
+
+    }
+    function not_entered() {
+
+    }
+    function not_left() {
+
+    }
+
+    function moved() {
+        console.log("Move");
+        d3.event.preventDefault();
+        const xm = x.invert(d3.event.layerX) - 4;
+        var data_key = []
+        for (d in data) {
+            data_key.push(d);
+        }
+        const i1 = d3.bisectLeft(data_key, xm, 1);
+        const i0 = i1 - 1;
+        const i = xm - data_key[i0] > data_key[i1] - xm ? i1 : i0;
+        dot.attr("transform", `translate(${x(data[i].x_value)},${y(data[i].y_value)})`);
+        dot.select("text").text('巡目' + data[i].x_value + ": " + data[i].y_value.toFixed(2));
+    }
+
+    function entered() {
+        console.log("Enter");
+        dot.attr("display", null);
+    }
+
+    function left() {
+        console.log("Left");
+        dot.attr("display", "none");
+    }
+}
+
+// update chart
+function update_chart(s) {
+    svg.call(hover, LineDataset[s], false);
+    update(LineDataset[s]);
+    update_y_label(s);
+    setTimeout(() => { svg.call(hover, LineDataset[s], true); }, 2000);
+}
